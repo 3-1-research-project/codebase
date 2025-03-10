a4c0643310d3ec33a8f32aef3dede717f2283faf
LVOL98 <lukas98@live.dk>
2025-03-10 11:38:57 +0000

    Updating gh actions with new test case

--- tests/helpers/test_helper.py
+++ tests/helpers/test_helper.py
@@ -1,6 +1,8 @@
+import os
 import random
 import string
 
+BASE_URL = os.environ["BASE_URL"]
 
 def generate_random_username():
     return "".join(random.choice(string.ascii_letters) for i in range(8))
