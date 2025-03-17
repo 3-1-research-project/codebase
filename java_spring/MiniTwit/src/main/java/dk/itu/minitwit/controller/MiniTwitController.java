e5e47393b09b0a11881a910b0c19b88d862d6c42
nickarlsen <nkar@itu.dk>
2025-03-17 13:14:47 +0100

    updated to fix flashmessage

--- java_spring/MiniTwit/src/main/java/dk/itu/minitwit/controller/MiniTwitController.java
+++ java_spring/MiniTwit/src/main/java/dk/itu/minitwit/controller/MiniTwitController.java
@@ -402,7 +402,10 @@ public class MiniTwitController {
                 return template;
             }
         }
-
+        if (logout != null) {
+            redirectAttributes.addFlashAttribute("flashMessage", "You were logged out");
+            return "redirect:/public";
+        }
         String template = "login.html";
         logger.info("Request ID: %s -- Returning template: %s".formatted( model.getAttribute("requestID"), template));
         return template;
